4aa92952ebf89a233b3e2ab0ba1e5782da25a7de
Alberto Calzada González-Román <alberto.calzada@educa.madrid.org>
2024-03-16 16:21:49 +0100

    Configuraciones basicas express

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+const {config}= require ('dotenv') //para cargar las variables desde un archivo .env
+config()
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.listen(port,()=>{
+    console.log(`Servidor escuchando en el puerto: ${port}`)
+})
+
+
