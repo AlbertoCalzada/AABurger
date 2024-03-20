cb6cd66cfd5c21fe847b32c0f1eadfdd2a9d75d1
Alberto Calzada González-Román <alberto.calzada@educa.madrid.org>
2024-03-20 17:41:45 +0100

    Login back creado

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -34,5 +34,42 @@ export const register = async (req, res) => {
 
 
 }
-export const login = (req, res) => res.send('login')
+export const login = async (req, res) => {
+
+    const { username, password } = req.body
+
+
+    try {
+
+        const userFound = await User.findOne({ username }) 
+
+        if (!userFound) {
+            return res.status(400).json({ message: 'Usuario no encontrado' })
+        }
+
+        const isEqual = await bcrypt.compare(password, userFound.password) //Para comparar contraseñas hasheadas
+
+        if (!isEqual) {
+            return res.status(400).json({ message: 'Contraseña incorrecta' })
+        }
+
+        
+        const token = await createAccessToken({ id: userFound._id })
+
+        res.cookie('token', token)  //establece la cookie token, metodo ya hecho
+
+        //le mandamos al front estos datos
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+
+
+}
+
 
--- server/db.js
+++ server/db.js
@@ -6,6 +6,7 @@ export const connectDB = async () => {
         await mongoose.connect('mongodb://localhost:27017/prueba_restaurante')
         console.log('Te has conectado con exito a la bbdd')
     } catch (error) {
+        
         console.log(error)
     }
 }
