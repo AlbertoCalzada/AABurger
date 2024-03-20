be51be9683a7e394961e1ef86400cfd0ffef9e26
Krig <albertoguerra4@gmail.com>
2024-03-20 19:10:55 +0100

    Posible footer a esperar unos retoques

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,8 +2,56 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Footer(){
+export default function Footer() {
+    const currentYear = new Date().getFullYear();
     return (
-        <h1>Trabajando en footer</h1>
+        <footer className="text-gray-600 body-font">
+            <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+                <Image src="/img/icon.svg" alt="Logo Web" width={800/10} height={800/10}/>
+                {/*<span className="ml-3 text-xl">A&A Burger</span>*/}
+                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 mx-20">
+                    © {currentYear} - Todos los derechos reservados
+                </p>
+                <div className="flex flex-col sm:flex-row mt-4 sm:mt-0">
+            <input type="email" placeholder="Correo electrónico" className="w-full sm:w-auto bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">Todas nuestras novedades</p>
+            <button className="mt-2 sm:mt-0 sm:ml-2 inline-flex items-center justify-center px-1 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Suscribirse
+            </button>
+        </div>
+                <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    <Link href="https://www.facebook.com/" target="_blank">
+                        <button className="text-gray-500 hover:text-blue-500">
+                            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                            </svg>
+                        </button>
+                    </Link>
+                    <Link href="https://twitter.com/" target="_blank">
+                        <button className="ml-3 text-gray-500 hover:text-blue-500">
+                            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                            </svg>
+                        </button>
+                    </Link>
+                    <Link href="https://www.instagram.com/" target="_blank">
+                        <button className="ml-3 text-gray-500 hover:text-blue-500">
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                            </svg>
+                        </button>
+                    </Link>
+                    <Link href="https://www.linkedin.com/" target="_blank">
+                        <button className="ml-3 text-gray-500 hover:text-blue-500">
+                            <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                                <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                            </svg>
+                        </button>
+                    </Link>
+                </span>
+            </div>
+        </footer>
     );
 }
