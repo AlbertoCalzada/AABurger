961270448ee1c8f6467190bad5903fbc472dc3f2
Alberto Calzada González-Román <alberto.calzada@educa.madrid.org>
2024-03-29 20:19:41 +0100

    pruebas carrusel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,20 @@
 import Navbar from '../components/navbar'
+import Carousel from '../components/carousel'
 import "../styles/globals.css"
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-      <head>      
-      <meta name='description' content='Web proyecto final de un restaurante' />
-      <link rel='icon' href='/img/favicon.ico' />
-      <title>A & A Burger</title>
-      </head>      
+      <head>
+        <meta name='description' content='Web proyecto final de un restaurante' />
+        <link rel='icon' href='/img/favicon.ico' />
+        <title>A & A Burger</title>
+      </head>
       <body>
         <Navbar />
+       
         {children}
+        <Carousel />
       </body>
     </html>
   )
