1dd2763d6090da1a30f1dd65fe4d83e7cadea2e0
Alberto Calzada González-Román <alberto.calzada@educa.madrid.org>
2024-05-06 13:24:09 +0200

    front apartado reservas

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,14 +1,24 @@
+'use client'
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Navbar() {
+  const handleReserveClick = (e) => {
+    e.preventDefault();
+    const reserveElement = document.getElementById('reserve');
+    if (reserveElement) {
+      reserveElement.scrollIntoView({ behavior: 'smooth' });
+    }else {
+      window.location.href = '/#reserve';
+    }
+  };
+
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link href='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <Image src="/img/icon.svg" alt="Logo Web" width={2000/10} height={2000/10}/>
-          {/*<span className="ml-3 text-xl">A&A Burger</span>*/}
         </Link>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
           <Link href="/menu" className="mr-5 hover:text-gray-900">Carta</Link>
@@ -19,7 +29,7 @@ export default function Navbar() {
         <Link href='/order'>
           <button className="inline-flex text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0">Pedir</button>
         </Link>
-        <Link href='/reserve'>
+        <Link href="#" onClick={handleReserveClick}>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 mx-2">Reservar
             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
---
+++ components/reservationForm.jsx
@@ -0,0 +1,116 @@
+'use client'
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+function ReservationForm() {
+    const [peopleCount, setPeopleCount] = useState(1);
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [selectedTurn, setSelectedTurn] = useState('lunch');
+
+    const availableTimes = {
+        lunch: [
+            '13:00', '13:15', '13:30', '13:45',
+            '14:00', '14:15', '14:30', '14:45',
+            '15:00', '15:15', '15:30', '15:45',
+            '16:00'
+        ],
+        dinner: [
+            '20:00', '20:15', '20:30',
+            '20:45', '21:00', '21:15', '21:30',
+            '21:45', '22:00', '22:15', '22:30',
+            '22:45', '23:00'
+        ]
+    };
+
+    const handleReservation = () => {
+        // Enviar los datos de la reserva al servidor
+        console.log('Reserva:', { peopleCount, date, time });
+    };
+
+    return (
+        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex flex-wrap justify-center lg:justify-start">
+                <div className="w-full lg:w-1/2 lg:pr-4 mb-4 lg:mb-0">
+                    <h2 className="text-3xl font-bold mb-6 text-blue-600 text-center">Realizar Reserva</h2>
+                    <div className="flex justify-center mb-6">
+                        <button
+                            className={`mr-4 px-6 py-3 rounded-lg transition-colors ${selectedTurn === 'lunch' ? 'bg-yellow-500 text-white hover:bg-yellow-600' : 'bg-gray-200 text-gray-700 hover:bg-gray-300'}`}
+                            onClick={() => setSelectedTurn('lunch')}
+                        >
+                            Turno de Comida
+                        </button>
+                        <button
+                            className={`px-6 py-3 rounded-lg transition-colors ${selectedTurn === 'dinner' ? 'bg-yellow-500 text-white hover:bg-yellow-600' : 'bg-gray-200 text-gray-700 hover:bg-gray-300'}`}
+                            onClick={() => setSelectedTurn('dinner')}
+                        >
+                            Turno de Cena
+                        </button>
+                    </div>
+                    <form>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="peopleCount">
+                                Cantidad de Personas:
+                            </label>
+                            <select
+                                id="peopleCount"
+                                value={peopleCount}
+                                onChange={(e) => setPeopleCount(parseInt(e.target.value))}
+                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            >
+                                {[...Array(8).keys()].map((index) => (
+                                    <option key={index} value={index + 1}>
+                                        {index + 1}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+                                Fecha:
+                            </label>
+                            <input
+                                type="date"
+                                id="date"
+                                value={date}
+                                onChange={(e) => setDate(e.target.value)}
+                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="time">
+                                Hora:
+                            </label>
+                            <select
+                                id="time"
+                                value={time}
+                                onChange={(e) => setTime(e.target.value)}
+                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            >
+                                {availableTimes[selectedTurn].map((timeOption) => (
+                                    <option key={timeOption} value={timeOption}>
+                                        {timeOption}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <button
+                            type="button"
+                            onClick={handleReservation}
+                            className="bg-gray-800 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline w-full transition duration-300 ease-in-out hover:bg-gray-700"
+                        >
+                            Reservar
+                        </button>
+
+                    </form>
+                </div>
+                <div className="w-full lg:w-1/2">
+                    <Image src="/images/fotoRestaurante.jpg" alt="Foto del restaurante" width={500} height={300} className="w-full h-auto rounded-lg hover:opacity-90 transition-opacity" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ReservationForm;
