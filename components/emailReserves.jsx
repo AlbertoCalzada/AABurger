3f3b2d76757cc1233d9c591bbb5d5148284ccb73
Alberto Calzada González-Román <alberto.calzada@educa.madrid.org>
2024-05-15 14:25:11 +0200

    working email

--- components/emailReserves.jsx
+++ components/emailReserves.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 const EmailTemplate = ({ name, time, date, peopleCount }) => {
   return (
     <div>
-      <h1>Muchas gracias por tu reserva, {name}!</h1>
+      <h1 style={{color: 'black'}}>Muchas gracias por tu reserva, {name}!</h1>
       <p>Tu reserva:</p>
-      <p>Fecha: {date}</p>
-      <p>Hora: {time}</p>
-      <p>Número de personas: {peopleCount}</p>
-      <p>¡Esperamos verte pronto!</p>
+      <p><span style={{color: 'black'}}>Fecha:</span> {date}</p>
+      <p><span style={{color: 'black'}}>Hora:</span> {time}</p>
+      <p><span style={{color: 'black'}}>Número de personas:</span> {peopleCount}</p>
+      <p><span style={{color: 'black'}}>¡Esperamos verte pronto!</span></p>
     </div>
   );
 };
+
 
 export default EmailTemplate;
