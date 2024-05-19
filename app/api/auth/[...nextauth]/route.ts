483b8fbadb5d43e3bbb07141774293da0bddace1
Alberto Calzada González-Román <alberto.calzada@educa.madrid.org>
2024-05-19 17:24:37 +0200

    working

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from '../../../../server/models/user.model';
 import bcrypt from 'bcryptjs';
+import { connectDB } from '../../../../server/db';
 
 const handler = NextAuth({
     providers: [GoogleProvider({
@@ -10,20 +11,24 @@ const handler = NextAuth({
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }), CredentialsProvider({
         // The name to display on the sign in form (e.g. "Sign in with...")
-        name: "Credentials",
+        name: "credentials",
         // `credentials` is used to generate a form on the sign in page.
         // You can specify which fields should be submitted, by adding keys to the `credentials` object.
         // e.g. domain, username, password, 2FA token, etc.
         // You can pass any HTML attribute to the <input> tag through the object.
         credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
-            password: { label: "Password", type: "password" }
+            username: { label: "Username", type: "text", placeholder: "Usuario" },
+            password: { label: "Password", type: "password" , placeholder: "Contraseña"}
         },
         async authorize(credentials, req) {
             try {
+
+                 // Conectar a la base de datos
+                 await connectDB();
+                 
                 // Buscar al usuario en la base de datos por su nombre de usuario
                 const user = await User.findOne({ username: credentials.username });
-        
+                console.log(user)
                 if (!user) {
                     throw new Error('Usuario no encontrado');
                 }
