b8909fb9ac7c691dbb3c656b211ebf6aa5f3dee8
Alberto Calzada González-Román <alberto.calzada@educa.madrid.org>
2024-05-30 00:48:33 +0200

    api/auth route

--- app/api/auth/auth.js
+++ app/api/auth/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API = "http://localhost:8000/api";
-//const API = "https://restaurant-app-blush-kappa.vercel.app/api";
+//const API = "http://localhost:8000/api";
+const API = "https://restaurant-app-blush-kappa.vercel.app/api";
 
 export const registerRequest = user => axios.post(`${API}/register`, user)
 
